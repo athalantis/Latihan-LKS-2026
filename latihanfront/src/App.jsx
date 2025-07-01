151718270784941ac11bc39fc23e5a6a22449ee1
athalantis <zaskianasywa97@gmail.com>
2025-07-01 09:20:33 +0700

    update1

---
+++ latihanfront/src/App.jsx
@@ -0,0 +1,13 @@
+import Profil from "./components/profil" // nah ini kita ambil dari components biar functionnya keambil
+
+function App() {
+
+  return (
+    <div>
+      <h1>hai aku lagi belajar react</h1>
+      <Profil/> {/* ini dia function yang dipanggil */}
+    </div>
+  )
+}
+
+export default App
---
+++ latihanfront/src/components/Profil.jsx
@@ -0,0 +1,12 @@
+// lagi nyoba function untuk dipanggil di App.jsx nanti
+
+function Profil({nama, keahlian}) {
+    return (
+        <div>
+            <h2>profil orang keren</h2>
+            <p>akulah si keren itu</p>
+        </div>
+    );
+}
+
+export default Profil;
